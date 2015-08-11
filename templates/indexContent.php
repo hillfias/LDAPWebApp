c715aaeb744def692f896a092d959fb174090e57
William Fuchs <fuchswilli@eisti.eu>
2015-08-11 11:35:40 +0200

    tuto+icones MAJ

--- templates/indexContent.php
+++ templates/indexContent.php
@@ -4,15 +4,30 @@
 		<div class="main-panel" id=""><!--large-9 columns-->
 			<!--main panel, ici c'est les commandes et tout ... -->
 			<h1>Mini-tutoriel d'administration</h1>
-			<p>Bienvenue, Admin ! Vous pouvez :
+			<p>Bienvenue, Admin ! Vous avez le choix entre un menu 'Groupes' et un menu 'Utilisateurs' (Il suffit de cliquer sur respectivement 'Groupes' ou 'Utilisateurs' pour changer de menu).</p>
+			
+			<p>Dans la barre du haut vous avez deux boutons : 	<?php echo '<img src="'.$CONSTANTES['cheminImages'].'addGroupe.svg" title="Ajouter un groupe" alt="Ajouter un groupe" width="30px"/>'; ?> et <?php echo '<img src="'.$CONSTANTES['cheminImages'].'addUser.svg" title="Ajouter un utilisateur" alt="Ajouter un utilisateur" width="30px"/>'; ?>.<br />
+			Le premier sert à ajouter un nouveau groupe, le deuxième est pour ajouter un autre utilisateur.</p>
+			<p>Dans le menu 'Groupes' (plus complexe) vous pouvez :
 			<ul>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'boutonPlus.png" title="Ajouter un utilisateur" alt="Ajouter un utilisateur" width="30px"/>'; ?>Ajouter un groupe ou un utilisateur</li>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'iconeGroupe.png" alt="Groupe" title="groupe" width="30px"/>'; ?>Modifier un groupe</li>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'boutonRemove.png" alt="Utilisateur" title="Utilisateur" width="30px"/>'; ?>Supprimer un groupe</li>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'change.png" alt="Changer l\'admin" title="Changer l\'admin" width="30px"/>'; ?>Changer l'administrateur d'un groupe</li>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'profil.png" alt="Utilisateur" title="Utilisateur" width="30px"/>'; ?>Modifier un utilisateur</li>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'boutonRemove.png" alt="Supprimer un utilisateur" title="Supprimer un utilisateur" width="30px"/>'; ?>Supprimer un utilisateur</li>
-				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'kick2.png" alt="Enlever un utilisateur" title="Enlever un utilisateur" width="30px"/>'; ?>Enlever un utilisateur d'un groupe</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'groupe.svg" title="Groupe" alt="Groupe" width="30px"/>'; ?>Voir la liste des utilisateurs d'un groupe</li>
+				<li>Modifier un groupe (cliquer sur le nom du groupe)</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'addAdmin.svg" title="Ajouter un admin" alt="Ajouter un admin" width="30px"/>'; ?>Ajouter un admin pour ce groupe</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'deleteGroupe.svg" title="Supprimer un groupe" alt="Supprimer un groupe" width="30px"/>'; ?>Supprimer un groupe</li>
+				
+				Une fois la liste des utilisateurs d'un groupe affichée vous remarquerez que les admins d'un groupe sont repérés par l'image admin <?php echo '<img src="'.$CONSTANTES['cheminImages'].'admin.svg" alt="Admin" title="Admin" width="30px"/>'; ?>
+				
+				
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'removeAdmin.svg" alt="Supprimer l\'admin" title="Supprimer l\'admin" width="30px"/>'; ?>Vous pouvez à ce moment là supprimer ses droits en tant qu'administrateur du groupe (Il vous sera demandé d'en ajouter un autre s'il est unique)</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'profil.png" alt="Utilisateur" title="Utilisateur" width="30px"/>'; ?>Modifier un utilisateur (cliquer sur son image de profil ou son nom)</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'deleteUser.svg" alt="Supprimer un utilisateur" title="Supprimer un utilisateur" width="30px"/>'; ?>Supprimer un utilisateur (lorsque vous développez le groupe 'all')</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'removeUser.svg" alt="Enlever un utilisateur" title="Enlever un utilisateur" width="30px"/>'; ?>Enlever un utilisateur d'un groupe (pour tous les autres groupes )</li>
+			</ul>
+			</p>
+			<p>Dans le menu 'Utilisateurs' (plus simple) vous avez une vue globale de tous les utilisateurs pour pouvoir simplement :
+			<ul>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'profil.png" alt="Utilisateur" title="Utilisateur" width="30px"/>'; ?>Modifier un utilisateur (cliquer sur son image de profil ou son nom)</li>
+				<li><?php echo '<img src="'.$CONSTANTES['cheminImages'].'deleteUser.svg" alt="Supprimer un utilisateur" title="Supprimer un utilisateur" width="30px"/>'; ?>Supprimer un utilisateur</li>
 			</ul>
 			</p>
 			
--- templates/indexInfoGroup.php
+++ templates/indexInfoGroup.php
@@ -1,17 +1,19 @@
 <?php
-	echo '<dt><a href="bla" class="deleteGroupe"><img src="'.$CONSTANTES['cheminImages'].'deleteGroupe.svg" alt="Supprimer Groupe" title="Supprimer Groupe" width="25px"/></a>';
-	if($nbgroup > 0 )
-	echo '<a href="bla" class="deleteGroupe"><img src="'.$CONSTANTES['cheminImages'].'addAdmin.svg" alt="Ajouter un administrateur pour ce groupe" title="Ajouter un administrateur pour ce groupe" width="25px"/></a>';
-	
-	
-	echo '<a class="accordionTitle" href="#">';
-	if($nbgroup > 0 ) echo '<span href="yoyo" title="Modifier ce groupe">';
-	echo '<strong>'.$infoNbGroupes[$nbgroup]['cn'][0].'</strong>';
-	if($nbgroup > 0 ) echo '</span>';
-	echo '</a>';
-	
-	
-	echo '</dt>';
-	echo "\n";
-	echo '<dd class="accordionItem accordionItemCollapsed">';
+echo '<dt>';
+if($nbgroup > 0 )
+echo '<a href="bla" class="deleteGroupe"><img src="'.$CONSTANTES['cheminImages'].'deleteGroupe.svg" alt="Supprimer Groupe" title="Supprimer Groupe" width="25px"/></a>';
+
+echo '<a href="bla" class="deleteGroupe"><img src="'.$CONSTANTES['cheminImages'].'addAdmin.svg" alt="Ajouter un administrateur pour ce groupe" title="Ajouter un administrateur pour ce groupe" width="25px"/></a>';
+
+
+echo '<a class="accordionTitle" href="#">';
+if($nbgroup > 0 ) echo '<span href="yoyo" title="Modifier ce groupe">';
+echo '<strong>'.$infoNbGroupes[$nbgroup]['cn'][0].'</strong>';
+if($nbgroup > 0 ) echo '</span>';
+echo '</a>';
+
+
+echo '</dt>';
+echo "\n";
+echo '<dd class="accordionItem accordionItemCollapsed">';
 ?>
